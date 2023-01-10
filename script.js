4a8a0b2ee5aeea9e07072e1bb0cfa6f26ad1e8fa
Yusen22 <elijahwilsher@gmail.com>
2023-01-10 13:16:34 +0000

    implemented alerts for different character types

--- script.js
+++ script.js
@@ -1,122 +1,129 @@
 // Array of special characters to be included in password
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
+var characters = {
 
-// Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  specialCharacters: [
+    '@',
+    '%',
+    '+',
+    '\\',
+    '/',
+    "'",
+    '!',
+    '#',
+    '$',
+    '^',
+    '?',
+    ':',
+    ',',
+    ')',
+    '(',
+    '}',
+    '{',
+    ']',
+    '[',
+    '~',
+    '-',
+    '_',
+    '.'
+  ],
 
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
+  // Array of numeric characters to be included in password
+  numericCharacters: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
 
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
+  // Array of lowercase characters to be included in password
+  lowerCasedCharacters: [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ],
+
+  // Array of uppercase characters to be included in password
+  upperCasedCharacters: [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+  ],
+}
 
 var fullCharSet = []
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  {
-    var passwordLength = prompt("How many characters should the password contain? Must be between 10 & 64 characters.");
-    if (passwordLength < 10) {
-      alert("This password has too few characters. Please enter another value.");
-      passwordLength = prompt("How many characters should the password contain? Must be between 10 & 64 characters.");
-    } else if (passwordLength > 64) {
-      alert("This password has too many characters. Please enter another value.");
-      passwordLength = prompt("How many characters should the password contain? Must be between 10 & 64 characters.");
-    } else {
-      characterTypeSelect(specialCharacters);
-      characterTypeSelect(numericCharacters);
-      characterTypeSelect(lowerCasedCharacters);
-      characterTypeSelect(upperCasedCharacters);
-    }
+  var passwordLength = prompt("How many characters should the password contain? Must be between 10 & 64 characters.");
+  if (passwordLength < 10) {
+    alert("This password has too few characters. Please enter another value.");
+    passwordLength = prompt("How many characters should the password contain? Must be between 10 & 64 characters.");
+  } else if (passwordLength > 64) {
+    alert("This password has too many characters. Please enter another value.");
+    passwordLength = prompt("How many characters should the password contain? Must be between 10 & 64 characters.");
+  } else {
+    characterTypeSelect();
   }
 }
 
 // Function for asking user about inclusion of different character types
 
-function characterTypeSelect(charset) {
-  var charDecision = confirm("Do you want to include " + charset + " ?");
-  if (charDecision === true) {
-    fullCharSet = fullCharSet.concat(charset);
-    alert(charset + " will be generated in the password.")
-  } else return
+
+var listProperty = Object.keys(characters);
+
+
+function characterTypeSelect() {
+  for (let i in characters) {
+    var charTitle = listProperty.shift();
+    var editedCharTitle = charTitle.replace(/[A-Z]/g, ' $&').trim().toLowerCase();
+    var charDecision = confirm("Do you want to include " + editedCharTitle + " ?");
+    //   if (charDecision === true) {
+    //     fullCharSet = fullCharSet.concat(characters[i]);
+    //     alert(characters[i] + " will be generated in the password.")
+    //   } else return
+    // }
+  }
 }
 
 // Function for getting a random element from an array
